befc3bcfde0c10b67b2ddda1f69688648f0acc1d
Payam Beigi <beigi.payam@gmail.com>
2022-11-14 22:45:52 -0800

    add projects

--- src/components/PayamBeigi/Projects.jsx
+++ src/components/PayamBeigi/Projects.jsx
@@ -27,7 +27,7 @@ export default function ProjectsBlock() {
         <p className='section-title'>Web 2.0 Development Projects</p>
         <div className="inner-container s1 border background-reverse">
           <div className={`image-card-left ${P1}`}>
-            <img className="gif-showcase" src='/media/img/MLD.gif' alt='testing' onClick={handleHoverLeftP1} />
+            <img className="gif-showcase" src='/media/img/portfolio.gif' alt='testing' onClick={handleHoverLeftP1} />
           </div>
           <div className="description-card-right background-reverse">
             <div className='project-description-container'>
@@ -36,7 +36,7 @@ export default function ProjectsBlock() {
                 My portfolio built with React.js. Incorporated an extensive varity of stylings, designs, and effects to enhance my front-end skills.
               </div>
               <div className='button-wrapper background text'>
-                <a href='https://github.com/diavolosz/website-mleduong.com' className='button-to-project' target="_blank">GitHub</a>
+                <a href='https://github.com/' className='button-to-project' target="_blank">GitHub</a>
               </div>
             </div>
           </div>
@@ -46,33 +46,33 @@ export default function ProjectsBlock() {
         <div className="inner-container s2 border background-reverse">
           <div className="description-card-left background-reverse">
             <div className='project-description-container'>
-              <div className='project-title text-reverse'> BayStreetbets </div>
+              <div className='project-title text-reverse'> FitPal </div>
               <div className='project-description text-reverse'>
-                A Stock Trading Platform that allow user to keep track of their stocks and portfolios. Has unique feature for trade competitions with peers to crown the winner.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur nostrum veniam eveniet, dolorem fuga nam sint in asperiores reiciendis ut, sapiente iure cupiditate expedita distinctio. Unde fuga accusamus libero recusandae!
               </div>
               <div className='button-wrapper background text'>
-                <a href='https://github.com/diavolosz/BayStreetBets' className='button-to-project' target="_blank">GitHub</a>
+                <a href='https://github.com/' className='button-to-project' target="_blank">GitHub</a>
               </div>
             </div>
           </div>
           <div className={`image-card-right ${P2}`}>
-            <img className="gif-showcase" src='/media/img/BBS.gif' alt='testing' onClick={handleHoverRightP2} />
+            <img className="gif-showcase" src='/media/img/FitPal.gif' alt='testing' onClick={handleHoverRightP2} />
           </div>
         </div>
 
 
         <div className="inner-container s3 border background-reverse">
           <div className={`image-card-left ${P3}`}>
-            <img className="gif-showcase" src='/media/img/PNT.gif' alt='testing' onClick={handleHoverLeftP3} />
+            <img className="gif-showcase" src='/media/img/DecisionMaker.gif' alt='testing' onClick={handleHoverLeftP3} />
           </div>
           <div className="description-card-right background-reverse">
             <div className='project-description-container'>
-              <div className='project-title text-reverse'> Pinnet </div>
+              <div className='project-title text-reverse'> DecisionMaker </div>
               <div className='project-description text-reverse'>
-                An Address Notebook. Pinnet offers users the ability to pin and save their favorite place to eat or hangout by creating their own sharable address list.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nesciunt sunt ea, voluptatem suscipit perferendis ipsa, sit assumenda totam possimus corporis qui omnis numquam repudiandae accusantium earum voluptas ad dolor?
               </div>
               <div className='button-wrapper background text'>
-                <a href='https://github.com/diavolosz/Lighthouse-MT-wikiMaps' className='button-to-project' target="_blank">GitHub</a>
+                <a href='https://github.com/' className='button-to-project' target="_blank">GitHub</a>
               </div>
             </div>
           </div>
@@ -83,15 +83,15 @@ export default function ProjectsBlock() {
             <div className='project-description-container'>
               <div className='project-title text-reverse'> Jungle </div>
               <div className='project-description text-reverse'>
-                A mini E-commerce. Built with Ruby on Rails, the application allows visitors to view, purchase plants while allowing seller to stock up inventory and sell.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos veniam saepe ex cumque incidunt voluptatem, eius est nobis eveniet aliquam minima veritatis dolorum labore corporis sapiente perferendis mollitia, ad quo.
               </div>
               <div className='button-wrapper background text'>
-                <a href='https://github.com/diavolosz/RubyRails-jungle-project' className='button-to-project' target="_blank">GitHub</a>
+                <a href='https://github.com/' className='button-to-project' target="_blank">GitHub</a>
               </div>
             </div>
           </div>
           <div className={`image-card-right ${P4}`}>
-            <img className="gif-showcase" src='/media/img/JUN.gif' alt='testing' onClick={handleHoverRightP4} />
+            <img className="gif-showcase" src='/media/img/jungle.gif' alt='testing' onClick={handleHoverRightP4} />
           </div>
         </div>
 
