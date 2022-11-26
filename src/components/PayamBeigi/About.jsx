832407d36a08823dddd435a4af4dc5a93ee72fdd
Payam Beigi <beigi.payam@gmail.com>
2022-11-25 23:34:16 -0800

    Add about Section

--- src/components/PayamBeigi/About.jsx
+++ src/components/PayamBeigi/About.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import Paper from '@mui/material/Paper';
 import '../../styles/about.css'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const About = () => {
   return (
-    <>
-     <div id='About' className='about-section'>
-          Hello!
-          My name is Jeff Bridges and I'm a passionate Full-Stack Developer. I pivoted from being a Certified HVAC/R Technician due to always having an interest in problem solving and bringing ideas to life. With Web Development, I can nurture my creativity through a new medium and I'm excited to build intuitive, responsive, and efficient web applications. I graduated from Lighthouse Labs Bootcamp in August 2022 and am looking to build upon the experiences I gained there. Currently I'm learning C# and refining the skills already learned so that I can begin work on my next project!
-
-          I'd love to hear from you. Whether it's a job opportunity, a project collaboration, or just to chat. Please feel free to contact me.
-
-          ~ Don't Try To Be The Best. Just Try To Be 1% Better Everyday.
-    </div>
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Grid xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
